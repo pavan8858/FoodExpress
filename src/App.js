0cd96fa9b1a28968b9c779b561a80a2fd5b9f598
pavan8858 <pavan8858784804@gmail.com>
2025-01-20 15:54:59 +0530

    toggel correction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
+import { lazy,Suspense, useEffect, useState } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestauranMenu from "./components/RestaurantMenu";
+import UserContext from './components/UserContext.js';
+import { Provider } from "react-redux";
+
+import { createBrowserRouter, RouterProvider , Outlet} from "react-router-dom";
+import appStore from "./utils/appStore.js";
+import Cart from "./components/Cart.js";
+// import Grocery from "./components/Grocery";
+
+//chunking 
+//code spiting 
+//dynamic bundling 
+//lazy loading 
+//on demond loading 
+//dynamic import 
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
+    const [userName , setuserName] = useState();
+
+    // Authentication 
+    useEffect(()=>{
+        const data = {
+            name:"Pavan Gupta ",
+        };
+        setuserName(data.name);
+
+    },[]);
     return (
-        <div className="app">
-            <Header />
-            <Body />
-        </div>
+        <Provider store ={appStore}>
+        <UserContext.Provider value = {{loggedInUser : userName , setuserName}}>
+            <div className="app" >
+                <UserContext.Provider value = {{loggedInUser : "Narendra Modi"}}>
+                <Header />
+                </UserContext.Provider>
+               
+                
+                
+                <Outlet/>
+            </div>
+            </UserContext.Provider>
+            </Provider>
+        
+
     );
 };
 
@@ -18,11 +58,32 @@ const approuter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
+        children:[
+            {
+                path:"/",
+                element:<Body/>,
+            },
+            {
+                path: "/about",
+                element: <About />,
+                
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback = {<h1>Loading...</h1>}> <Grocery/> </Suspense>,
+            },
+            {
+                path:"/restaurant/:resId",
+                element:<RestauranMenu/>,
+            },
+            {
+                path:"/cart",
+                element:<Cart/>,
+            },
+        ],
+        errorElement:<Error/>,
     },
-    {
-        path: "/about",
-        element: <About />,
-    },
+
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,41 @@
+import React from "react";
+import UserClass from "./userClass";
+import UserContext from "./UserContext"; // Ensure this path is correct
 
-const About = ()=>{
-    return(
-        <div>
-            <h1>these is about page </h1>
-        </div>
-    )
-};
-export default About ;
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        // console.log("parentdid mount  called");
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> About class Component </h1>
+                <div>
+                    <UserContext.Consumer>
+                        {({loggedInUser}) => <h3>LoggedIn User : {loggedInUser}</h3>}
+                    </UserContext.Consumer>
+                </div>
+                <UserClass 
+                    name={"Krishna (class base component props)"} 
+                    location={"New Delhi"} 
+                    contact={"Krishna@gmail.com"} 
+                /> 
+            </div>
+        );
+    }
+}
+
+export default About;
+
+  
+
+        
+  
+
+
+
